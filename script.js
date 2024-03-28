dc4ffa2be15be86bc39846fd5464c7e78fc5c7f7
AntonisDela <antonisdelavogias@icloud.com>
2024-03-28 17:12:05 +0200

    created a function handleHover, each time the user mouse overs a certain nav section it gets highlighted and the rest drop the opacity. also moved all the dom variables to the top of the script

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
-
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 ///////////////////////////////////////
 // Modal window
 
@@ -49,14 +52,9 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
-//Tabbed component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
-
+  console.log(e.target);
   // Guard clause
   if (!clicked) return;
 
@@ -72,3 +70,21 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
